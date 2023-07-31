aec1bcdb1ae7cd48558442dd287beb6585edd6aa
Nick Stalter <nickstalter@gmail.com>
2023-07-30 23:30:07 -0400

    Add more functionality to main context

--- lib/blitz.ex
+++ lib/blitz.ex
@@ -10,9 +10,10 @@ defmodule Blitz do
   @spec main(summoner_name :: summoner_name(), region :: Http.region()) ::
           list(summoner_name()) | {:error, String.t()}
   def main(summoner_name, region) do
-    with {:ok, summoner_id} <- get_summoner_by_name(summoner_name, region),
-         {:ok, match_ids} <- Http.fetch_recent_match_ids_for_summoner(summoner_id, region),
+    with {:ok, summoner} <- get_summoner_by_name(summoner_name, region),
+         {:ok, match_ids} <- get_recent_matches_for_summoner(summoner.id, region),
          {:ok, recent_players} <- recent_players_from_match_ids(match_ids, region) do
+      # TODO: return the player names
       recent_players
     else
       error ->
@@ -21,9 +22,13 @@ defmodule Blitz do
   end
 
   def get_summoner_by_name(summoner_name, region) do
-    with {:ok, %{"puuid" => summoner_id}} <- Http.fetch_summoner(summoner_name, region) do
-      {:ok, summoner_id}
+    with {:ok, summoner} <- Http.fetch_summoner(summoner_name, region) do
+      {:ok, summoner}
     end
+  end
+
+  def get_recent_matches_for_summoner(summoner_id, region, match_count \\ 5) do
+    Http.fetch_recent_match_ids_for_summoner(summoner_id, region, match_count)
   end
 
   def recent_players_from_match_ids(match_ids, region) do
@@ -36,11 +41,13 @@ defmodule Blitz do
     {:ok, recent_players}
   end
 
+  # TODO: add json decoding from payloads
   def recent_players_from_match_id(match_id, region) do
     with {:ok, match} <- Http.fetch_match(match_id, region) do
       match
-      |> Map.get("metadata")
+      |> Map.get("info")
       |> Map.get("participants")
+      |> Enum.map(&Map.get(&1, "summonerName"))
     end
   end
 end
--- test/blitz_test.exs
+++ test/blitz_test.exs
@@ -1,8 +1,35 @@
 defmodule BlitzTest do
-  use ExUnit.Case
-  doctest Blitz
+  use Blitz.Case, async: true
+  alias Blitz.HttpMock
 
-  test "greets the world" do
-    assert Blitz.hello() == :world
+  setup :verify_on_exit!
+
+  setup do
+    name = username()
+    %{puuid: summoner_id} = summoner = build(:summoner, name: name)
+    region = region_id()
+    player_list = player_list()
+    matches = build_list(5, :match)
+    %{metadata: %{match_id: match_id}} = match = hd(matches)
+
+    HttpMock
+    |> stub(:fetch_summoner, fn ^name, ^region -> {:ok, summoner} end)
+    |> stub(:fetch_recent_match_ids_for_summoner, fn ^summoner_id, ^region, 5 ->
+      {:ok, matches}
+    end)
+    |> stub(:fetch_match, fn ^match_id, ^region -> {:ok, match} end)
+
+    ~M{name, summoner, summoner_id, region, player_list, matches}
+  end
+
+  test "fetching summoner by name and region", ~M{summoner, name, region} do
+    assert {:ok, summoner} == Blitz.get_summoner_by_name(name, region)
+  end
+
+  test "get_recent_matches_for_summoner", ~M{summoner_id, region, matches} do
+    assert {:ok, matches} == Blitz.get_recent_matches_for_summoner(summoner_id, region)
+  end
+
+  test "fetching a match returns the participants" do
   end
 end
